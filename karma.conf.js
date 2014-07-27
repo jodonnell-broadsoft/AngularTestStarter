fae4ad1fa57655f08bead20c92c4927cc92a9110
jonathanodonnell <jonathan.carl.odonnell@gmail.com>
2014-07-27 20:29:40 -0300

    Finally got this working with Karma!

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'spec/*Spec.js'
+        'src/*.js',
+        'spec/*.js'
     ],
 
 
@@ -50,7 +51,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch: false,
 
 
     // start these browsers
--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -10,7 +10,6 @@ describe("Player", function() {
   it("should be able to play a Song", function() {
     player.play(song);
     expect(player.currentlyPlayingSong).toEqual(song);
-
     //demonstrates use of custom matcher
     expect(player).toBePlaying(song);
   });
